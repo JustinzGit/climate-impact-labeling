9cb45a8e42936fed18e6f561066ff04aff95e01c
Justin <justinms@outlook.com>
2020-07-26 14:54:13 -0500

    add render method

--- frontend/src/food.js
+++ frontend/src/food.js
@@ -5,4 +5,20 @@ class Food {
         this.barcode = barcode
         this.ingredients = ingredients
     }
+
+    renderProduct() {
+        let productDiv = document.getElementById("product")
+
+        productDiv.innerHTML += 
+        `
+        <h2>Product Information</h2>
+            <ul>
+                <li>Barcode: ${this.barcode}</li>
+                <li>Brand Owner: ${this.owner}</li>
+            </ul>
+
+            <p>Ingredients:</p>
+            <p>${this.ingredients}</p>
+        `
+    }
 }
