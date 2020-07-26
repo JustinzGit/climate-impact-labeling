42adf870d2b550189690110adb0e7e5fbaadc1fb
Justin <justinms@outlook.com>
2020-07-26 14:54:38 -0500

    fetch production information

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     createBarcodeForm()
 })
 
+const BASE_URL = "http://127.0.0.1:3000"
+
 function createBarcodeForm(){
     let barcodeForm = document.getElementById("barcode-form")
 
@@ -14,11 +16,28 @@ function createBarcodeForm(){
     </form>
     `
 
-    barcodeForm.addEventListener("submit", barcodeSubmission)
+    barcodeForm.addEventListener("submit", fetchProduct)
 }
 
 function barcodeSubmission(){
     event.preventDefault()
     let barcode = document.getElementById("barcode").value
     return barcode
+}
+
+function fetchProduct(){
+    let barcode = parseInt(barcodeSubmission())
+    
+    fetch(`${BASE_URL}/foods/${barcode}`)
+    .then(resp => {
+        if(!resp.ok) throw "Product Not Found"
+        return resp.json()
+    })
+    .then(food => {
+        let foodProduct = new Food(food.brand_owner, food.gtin_upc, food.ingredients)
+        foodProduct.renderProduct()
+
+    }).catch(error => {
+        document.getElementById("error").innerHTML = error
+    })
 }
