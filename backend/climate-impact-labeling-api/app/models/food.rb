29707caa23e3272f7e4f52944317e8a94b6ac6ad
Justin <justinms@outlook.com>
2020-07-26 11:59:27 -0500

    set primary key for models

--- backend/climate-impact-labeling-api/app/models/food.rb
+++ backend/climate-impact-labeling-api/app/models/food.rb
@@ -4,4 +4,6 @@ class Food < ApplicationRecord
 
     has_many :food_nutrients
     has_many :nutrients, through: :food_nutrients
+
+    self.primary_key = "fdc_id"
 end
--- backend/climate-impact-labeling-api/app/models/nutrient.rb
+++ backend/climate-impact-labeling-api/app/models/nutrient.rb
@@ -1,4 +1,6 @@
 class Nutrient < ApplicationRecord
     has_many :food_nutrients
     has_many :foods, through: :food_nutrients
+
+    self.primary_key = "nutrient_id"
 end
