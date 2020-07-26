acfdff235a8266edcf08a4d8776b95edff1d9713
Justin <justinms@outlook.com>
2020-07-26 13:34:04 -0500

    add function to add  barcode form

---
+++ frontend/index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    createBarcodeForm()
+})
+
+function createBarcodeForm(){
+    let barcodeForm = document.getElementById("barcode-form")
+
+    barcodeForm.innerHTML +=
+    `
+    <form>
+        <label for="barcode">Enter Product Barcode</label><br>
+        <p><input type="text" id="barcode"></p>
+        <input type="submit">
+    </form>
+    `
+}
