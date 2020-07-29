a91bc2f6bdb9b6f80bc6dfac33a51e5db47a791e
Justin <justinms@outlook.com>
2020-07-29 16:12:35 -0500

    function to search by name

--- backend/climate-impact-labeling-api/app/models/food.rb
+++ backend/climate-impact-labeling-api/app/models/food.rb
@@ -6,4 +6,9 @@ class Food < ApplicationRecord
     has_many :nutrients, through: :food_nutrients
 
     self.primary_key = "fdc_id"
+
+    # Search by product name
+    def self.search(name)
+        where("name LIKE (?)", "%#{name}%")
+    end
 end
