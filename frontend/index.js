cb0c2f4714785066d59d5adc6cfa4b9d8f7a9bb1
Justin <justinms@outlook.com>
2020-07-29 19:44:28 -0500

    add name attribute

--- frontend/index.js
+++ frontend/index.js
@@ -89,7 +89,7 @@ function fetchProduct(barcode){
     .then(food => {
         if(food === null) throw "Product Not Found"
 
-        let foodProduct = new Food(food.brand_owner, food.gtin_upc, food.ingredients)
+        let foodProduct = new Food(food.name, food.brand_owner, food.gtin_upc, food.ingredients)
         
         // Remove previous product data
         document.getElementById("product-data").innerHTML = ""
