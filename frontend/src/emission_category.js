1401ff0d1e14c1000358842695c69974c875bcca
Justin <justinms@outlook.com>
2020-08-04 14:56:37 -0500

    change range variable name to data range

--- frontend/src/emission_category.js
+++ frontend/src/emission_category.js
@@ -32,24 +32,24 @@ class EmissionCategory {
                 ]
         }]
 
-        let range = [10, 30]
+        let dataRange = [10, 30]
         let emissionCategoryDiv = document.getElementById("emission-category")
     
-        if (this.ghg < range[0]){
+        if (this.ghg < dataRange[0]){
             emissionCategoryDiv.innerHTML =
             `
                 <h3>Greenhouse Gas Emissions: <b style="color: #1aa260;">Low</b></h3>
                 <p>Choosing more products of this category lowers your carbon footprint!</p>
             `
         }
-        else if (this.ghg >= range[0] && this.ghg < range[1]){
+        else if (this.ghg >= dataRange[0] && this.ghg < dataRange[1]){
             emissionCategoryDiv.innerHTML =
             `
                 <h3>Greenhouse Gas Emissions: <b style="color: #ffc107;">Moderate</b></h3>
                 <p>Choosing products of this category are smart alternatives to those with high emissions</p>
             `
         }
-        else if (this.ghg >= range[1]){
+        else if (this.ghg >= dataRange[1]){
             emissionCategoryDiv.innerHTML =
             `
                 <h3>Greenhouse Gas Emissions: <b style="color: #de5246";>High</b></h3>
@@ -62,29 +62,29 @@ class EmissionCategory {
         new Chartkick.ColumnChart("product-emissions-chart", data, {legend: false})
 
         function assignColumnColor(data, value){
-            let range
+            let dataRange
         
             switch(data){
                 case "ghg":
-                    range = [10, 30]
+                    dataRange = [10, 30]
                     break
                 case "water":
-                    range = [40, 900]
+                    dataRange = [40, 900]
                     break
                 case "eutrophication":
-                    range = [10, 70]
+                    dataRange = [10, 70]
                     break
                 case "acid":
-                    range = [15, 90]
+                    dataRange = [15, 90]
                     break
                 case "land":
-                    range = [21, 60]
+                    dataRange = [21, 60]
                     break
             }
         
-            if (value < range[0]) return "#1aa260"
-            else if (value >= range[0] && value < range[1]) return "#ffc107"
-            else if (value >= range[1]) return "#de5246"
+            if (value < dataRange[0]) return "#1aa260"
+            else if (value >= dataRange[0] && value < dataRange[1]) return "#ffc107"
+            else if (value >= dataRange[1]) return "#de5246"
         }
     }
 }
