68073a0da620f47c36172679a17520585864cb6d
Justin <justinms@outlook.com>
2020-08-05 15:27:56 -0500

    remove food nutrients association

--- backend/climate-impact-labeling-api/app/models/food.rb
+++ backend/climate-impact-labeling-api/app/models/food.rb
@@ -1,9 +1,6 @@
 class Food < ApplicationRecord
     belongs_to :emission_category
 
-    has_many :food_nutrients
-    has_many :nutrients, through: :food_nutrients
-
     self.primary_key = "fdc_id"
 
     # Search by product name
